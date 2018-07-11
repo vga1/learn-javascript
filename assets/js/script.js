ebcb1be0dced68700b6637b1a7451ae2ef2b0882
vga1 <workday20112014@gmail.com>
2018-07-12 02:35:03 +0300

    Mazahell task 1 (#8)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,24 @@
 const app = new Vue({
-  el:'#app',
-  data:{
-    errors:[],
-    inputEmail: null,
-    inputPassword:null
-
+  el: '#app',
+  data: {
+    errors: [],
+    inputEmail: '',
+    inputPassword: ''
   },
-  methods:{
-    checkForm:function(e) {
-      this.errors = [];
-      if(!this.name) this.errors.push("Укажите имя.");
-      if(!this.this.email) {
-        this.errors.push("Укажите электронную почту.");
-      } else if(!this.validEmail(this.email)) {
-        this.errors.push("Укажите корректный адрес электронной почты.");        
-      }
-      if(!this.errors.length) return true;
-      e.preventDefault();
-    },
-    validEmail:function(email) {
-      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(email);
+  methods: {
+    signIn: function() {
+      // https://validatejs.org/#validators-length
+      console.log(
+        validate(
+          { from: 'null' },
+          {
+            from: {
+              email: true,
+              length: { maximum: 3 }
+            }
+          }
+        )
+      );
     }
   }
-})
+});
